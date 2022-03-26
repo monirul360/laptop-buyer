7e5c17f08c8aadcb15fc0225bcbeaf5923e6fcdf
monirul360 <monirulislam65361@gmail.com>
2022-03-26 16:01:30 +0600

    header part complate

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>Laptop buyer</h1>
+            <p>Best online marketplace</p>
+        </div>
+    );
+};
+
+export default Header;

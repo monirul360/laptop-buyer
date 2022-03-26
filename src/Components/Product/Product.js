e69e6afcaa07ab729ce91e68e15dc125d5ee70f5
monirul360 <monirulislam65361@gmail.com>
2022-03-26 17:39:18 +0600

    fortawesome add

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 const Product = (props) => {
     return (
@@ -6,7 +9,7 @@ const Product = (props) => {
             <img src={props.product.img} alt="" />
             <p>{props.product.name}</p>
             <div className='btn-group'>
-                <button>ADD TO CART</button>
+                <button>ADD TO CART &nbsp; <FontAwesomeIcon style={{ fontSize: "17px" }} icon={faShoppingCart} /></button>
             </div>
         </div>
     );

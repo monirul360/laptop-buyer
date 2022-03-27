8c8cc0def6586db2a00aa41a587e9751df6fcf67
monirul360 <monirulislam65361@gmail.com>
2022-03-27 20:18:41 +0600

    fast ans add

--- src/Components/Ans/Ans.js
+++ src/Components/Ans/Ans.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Ans.css'
 
 const Ans = () => {
     return (
         <div>
-            <h4>How react work</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, rerum!</p>
+            <div className="fast-ans">
+                <h4>How react work</h4>
+                <p>React ইউজার ইন্টারফেস তৈরির জন্য একটি জাভাস্ক্রিপ্ট লাইব্রেরি .
+                </p>
+                <p>React এর মাধ্যমে খুব সহযে  ইউজার ইন্টারফেস তৈরি করা যায় ।
+                </p>
+                <p>React এর ভিতর পিওর জাভাস্ক্রিপ্ট  কোড ও লিখা যায় ।
+                </p>
+                <p>আমরা যে কোড গুল লিখি React  সেই গুল কে জাভাস্ক্রিপ্ট   এতে কনভার্ট করে নিয়ে কাজ করে থাকে ।</p>
+                <p>React কম্পোনেন্ট একটি render() মেথড তৈরি করে যা ইনপুট ডেটা নেয় এবং কী দেখাতে হবে তা প্রদান করে। </p>
+                <p>এই ভাবে React  কাজ করে থাকে মুলুত</p>
+            </div>
+            <div className="secend-ans">
+
+            </div>
         </div>
     );
 };
